6a5160404d15d70395fad6e0a3927f30d99e7b0c
Sonira <sonira.shokohyan531@gmail.com>
2023-08-28 18:15:01 +0500

    new lesson

--- members/models.py
+++ members/models.py
@@ -5,3 +5,6 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)     #second colum  har colum dar database banap field yad mesha
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
--- members/urls.py
+++ members/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
     # path('chat/', views.chat, name='chat'),
     # path('images/', views.images, name='images'),
     # path('videos/', views.videos, name='videos'),
