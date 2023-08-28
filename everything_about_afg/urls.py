1e0cd20fa5586918eb98931d40870e745f9a480d
Sonira <sonira.shokohyan531@gmail.com>
2023-08-28 18:31:20 +0500

    yrls modified

--- everything_about_afg/urls.py
+++ everything_about_afg/urls.py
@@ -18,8 +18,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('members.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
